d6139bf9ac8dacc75a1ef018af984312a3534bda
appsmall <37024455+appsmall@users.noreply.github.com>
2019-05-29 17:47:18 +0530

    - add project

---
+++ ManageDifferentEnvironments/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ManageDifferentEnvironments
+//
+//  Created by apple on 26/04/19.
+//  Copyright © 2019 appsmall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NotifyContent/NotificationViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NotificationViewController.swift
+//  NotifyContent
+//
+//  Created by apple on 26/04/19.
+//  Copyright © 2019 appsmall. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
